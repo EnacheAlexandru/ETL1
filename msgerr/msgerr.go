10f9acbff5445e9bcfa6fe3d5b3e10c67910a520
EnacheAlexandru <enachealexandru00@yahoo.com>
2023-01-26 18:06:51 +0200

    Small change in predefined errors

--- msgerr/msgerr.go
+++ msgerr/msgerr.go
@@ -1,13 +1,15 @@
 package msgerr
 
-type Error string
+type MyError string
 
-func (e Error) Error() string {
+func (e MyError) Error() string {
 	return string(e)
 }
 
-const ErrorFileOpen = Error("failed opening file")
-const ErrorFileClose = Error("failed closing file")
-const ErrorFileCreate = Error("failed creating file")
+const (
+	ErrorFileOpen   = MyError("failed opening file")
+	ErrorFileClose  = MyError("failed closing file")
+	ErrorFileCreate = MyError("failed creating file")
 
-const ErrorChunkTooSmall = Error("chunks size should be at least 1")
+	ErrorChunkTooSmall = MyError("chunk size should be at least 1")
+)
